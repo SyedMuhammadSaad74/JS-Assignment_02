66f2872011fd491956ecb5984d8ef127022df67e
Syed Muhammad Saad <136969885+SyedMuhammadSaad74@users.noreply.github.com>
2023-10-21 14:45:37 +0500

    Add files via upload

---
+++ JS Ass_02/app.js
@@ -0,0 +1,163 @@
+// ----------------------------- CHAPTER : 05----------------------------
+// QUESTION#01
+// var num1=+prompt();
+// var num2=+prompt();
+// var c=num1+num2;
+// document.write(c);
+
+// QUESTION#02
+// [var num1=+prompt();
+// var num2=+prompt();
+// var c=num1-num2;
+// document.write(c);]
+
+// var num1=+prompt();
+// var num2=+prompt();
+// var c=num1*num2;
+// document.write(c);
+
+// var num1=+prompt();
+// var num2=+prompt();
+// var c=num1/num2;
+// document.write(c);
+
+// QUESTION#03@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// var a="Initial value is: ";
+// var b="5";
+// var c=a + b;
+// document.write(c);
+
+// QUESTION#04
+// var a="Total cost to buy"; 
+// var price=600;
+// var tick="tickets to movie is";
+// var currency="PKR";
+// var tickets=prompt();
+// var cal=price * tickets;
+// var print=a + " " +  tickets + "" + " " + tick + " " + cal+ currency;
+// document.write(print); 
+
+// QUESTION#05@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// var a=4;
+// var b=1;
+// if(b=1 , b<=10 , b++){
+    // c=a*b    
+    // }
+    // document.write(c);
+    
+    // QUESTION#06
+    // var F=prompt();
+    // var cal=(F-32)*5/9;
+    // var C=cal+"C";
+    // document.write(C);
+    
+// var C=prompt();
+// var cal=(C*9/5)+32;
+// var F=cal+"F";
+// document.write(F);
+
+// QUESTION#07@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// var a="Price of item 1 is: ";
+// var noOfItem1=prompt();
+// var aPrice=700;
+// var qun1="Quantity of item 1 is ";
+// var cal=a+(noOfItem1*aPrice);
+
+// var b="Price of item 2 is: ";
+// var cal=b+(noOfItem1*aPrice);
+// var noOfItem2=prompt();
+// var e="Price of item 2 is: ";
+// var qun2="Quantity of item 2 is ";
+// var bPrice=1000;
+
+// var ship="Shipping charges is ";
+// var charges=100;
+
+
+// document.write(a+ aPrice)
+// document.write(qun + noOfItem1);
+// document.write(e + bPrice);
+// document.write(qun2 + noOfItem2);
+// document.write(ship + charges);
+
+// QUESTION#08
+// var obtainMarks=prompt("Enter obtain marks");
+// var totalMarks=prompt("Enter total marks");
+// var cal=(obtainMarks/totalMarks)*100;
+// var percentage=cal;
+// document.write(percentage+"%");
+
+// QUESTION#09
+// var dollar = prompt("Enter dollar");
+// var calD = dollar * 104.80;
+// var saudiRiyal = prompt("Enter the saudi riyal")
+// var calR = saudiRiyal * 28;
+// document.write("The conversion of dollar into pakistani ruppess is: " + calD , "The conversion of Saudi Riyal into pakistani ruppess is: " + calR);
+
+// QUESTION#10
+// var a=20;
+// cal=((a+5)*10)/2;
+// document.write(cal);
+
+// QUESTION#11
+// var currentYear=2023;
+// var bithYear=prompt("Enter your birth year");
+// var cal=currentYear-bithYear;
+// document.write("Your age is: "+cal);
+
+// QUESTION#12
+// var radius=prompt("Enter the radius");
+// var circum=2 * 3.142*radius;
+// var area=3.142*radius*radius;
+// result="Radius of a circle is: "+radius;
+// document.write(result+"The Circumference is: "+circum+"The Area is: "+radius);
+
+// QUESTION#13
+// var a=prompt("Enter your favourite snacks: ");
+// var b=prompt("Enter your currnet age: ");
+// var c=prompt("Estimated maximum age: ");
+// var d="Amount of snacks per day:3";
+// var cal=(c-b)*3;
+// document.write("You will need "+ "" + cal + " " + a +"to last you until the ripe old age of"+ " " +c );
+
+
+// ----------------------------- CHAPTER :(06-09)----------------------------
+// QUESTION#1
+// var a=10;
+// document.write("The value of a is: "+ a);
+// document.write("The value of ++a is: "+ ++a);
+//  document.write("The value of a++ is: "+ a++);
+// document.write("The value of --a is: "+ ++a);
+//  document.write("The value of a-- is: "+ a++);
+
+// QUESTION#2
+
+// QUESTION#3
+// var a=prompt("Enter your name");
+// var b="Assalm u Alaikum"+ " " +a;
+// document.write(b);
+
+// QUESTION#5
+// var num = prompt("Enter Number") 
+// var i = 0;
+// for (i = 1; i <= 10; i++) {
+    //   document.write( num + " x " + i + " = " + num * i );
+    // }
+    
+    // QUESTION#6
+    // var a=prompt("Enter first subject name");
+    // var ao=prompt("Enter the marks")
+    // var b=prompt("Enter second subject name");
+    // var bo=prompt("Enter the marks")
+    // var c=prompt("Enter third subject name");
+    // var co=prompt("Enter the marks")
+    // var totalMarks=100;
+    // var obtainMraks=a+b+c;
+    // var per=((ao+bo+co)/300)*100;
+    // document.write("<h4>Subjects  Total Marks  Obtained Marks Percentage</h4>");
+    // document.write(a+ " " + 100 + " " + ao + " " +ao+"%");
+    // document.write(b+ " " + 100 + " " + ao + " " +ao+"%");
+    // document.write(c+ " " + 100 + " " + ao + " " +ao+"%");
+    // document.write(totalMarks+obtainMraks);
+    
+    // ----------------------------- CHAPTER :(09-11)----------------------------
